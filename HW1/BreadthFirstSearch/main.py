c9c24f979640a0e233cd8877d84e9cf492eb6294
Alynoser <43915463+Alynoser@users.noreply.github.com>
2019-09-03 20:56:00 -0800

    Split string
    I was able to split the string, but now i am trying to convert the list chr's to ints

--- HW1/BreadthFirstSearch/main.py
+++ HW1/BreadthFirstSearch/main.py
@@ -12,17 +12,18 @@ def makelist(user_start, user_end):
     new_start = []
     new_end = []
     for i in range(user_start):
-        new_start.append(int(user_start(i)))
+        new_start[i] = int(user_start[i])
     for i in range(user_end):
-        new_end.append(int(user_end(i)))
+        new_end[i] = int(user_end[i])
     return new_start, new_end
 
 
 user_start = input("Please enter a starting set\n")
 user_end = input("Please enter desired end set\n")
 
-
-user_start, user_end = makelist(user_start, user_end)
+user_start = list(user_start)
+print(user_start)
+# user_start, user_end = makelist(user_start, user_end)
 
 
 #user_start = checkparity(user_start)
