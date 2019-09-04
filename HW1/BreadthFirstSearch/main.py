813bfe7f767762426fb296191d58ee2cd238a2bc
Alynoser <43915463+Alynoser@users.noreply.github.com>
2019-09-03 21:00:10 -0800

    Convert to int
    was able to convert to ints

--- HW1/BreadthFirstSearch/main.py
+++ HW1/BreadthFirstSearch/main.py
@@ -8,21 +8,15 @@ def checkparity(user_start):
         return temp_start
 
 
-def makelist(user_start, user_end):
-    new_start = []
-    new_end = []
-    for i in range(user_start):
-        new_start[i] = int(user_start[i])
-    for i in range(user_end):
-        new_end[i] = int(user_end[i])
-    return new_start, new_end
+
 
 
 user_start = input("Please enter a starting set\n")
 user_end = input("Please enter desired end set\n")
-
+user_start = list(map(int, user_start))
 user_start = list(user_start)
-print(user_start)
+
+
 # user_start, user_end = makelist(user_start, user_end)
 
 
