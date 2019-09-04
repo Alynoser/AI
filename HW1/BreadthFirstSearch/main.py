10bebfd2e7c83c5d1573c7afdeb48bba4bdf3c94
Alynoser <43915463+Alynoser@users.noreply.github.com>
2019-09-03 22:03:24 -0800

    Parity checker finished?
    I think i have the parity checker finished. Need someone to check it

--- HW1/BreadthFirstSearch/main.py
+++ HW1/BreadthFirstSearch/main.py
@@ -1,24 +1,35 @@
 import sys
+import copy
 
-
-def checkparity(user_start):
-    temp_start = user_start
+def checkparity(user_start, user_end):
+    temp_start = copy.copy(user_start)
+    temp_end = copy.copy(user_end)
     if 0 in temp_start:
         temp_start.remove(0)
-        return temp_start
-
-
+    if 0 in temp_end:
+        temp_end.remove(0)
+    start_count = 0
+    end_count = 0
+    for i in range(len(temp_start)):
+        if temp_start[i] != i+1 and temp_start[i] > i+1:
+            start_count += (temp_start[i] - (i+1))
+    for i in range(len(temp_end)):
+        if temp_end[i] != i+1 and temp_end[i] > i+1:
+            end_count += (temp_end[i] - (i+1))
+    print(start_count)
+    print(end_count)
+    start_count = start_count % 2
+    end_count = end_count % 2
+    if start_count == end_count:
+        print("YAY")
+    else:
+        print("YOU SUCK!!!")
 
 
 
 user_start = input("Please enter a starting set\n")
 user_end = input("Please enter desired end set\n")
 user_start = list(map(int, user_start))
-user_start = list(user_start)
-
-
-# user_start, user_end = makelist(user_start, user_end)
-
-
-#user_start = checkparity(user_start)
+user_end = list(map(int, user_end))
+checkparity(user_start, user_end)
 
