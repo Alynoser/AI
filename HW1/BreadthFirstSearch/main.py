4fd223f2445faa26cc75a343dc3b8014bebd6cf6
Alynoser <43915463+Alynoser@users.noreply.github.com>
2019-09-04 21:32:03 -0800

    Added in the queue stuff from elysha, need to fix this whole thing going on git

--- HW1/BreadthFirstSearch/main.py
+++ HW1/BreadthFirstSearch/main.py
@@ -6,6 +6,40 @@ import copy
 #  is the end point given by the user
 
 
+# TODO: Create queue somewhere.
+
+# node: Object to be Queued
+# curr_queue: Current Queue
+# Input: Takes in a node and the list of currently queued items.
+# Output: Returns the new list
+def queue(node, curr_queue):
+    if not curr_queue.full():
+        curr_queue.put(node)
+    else:
+        print("Queue is full. Cannot accept more items.")
+    return curr_queue
+
+
+# curr_queue: Current Queue
+# Input: Takes in a list and de-queues the last item (if it exists).
+# Output: item from Queue and new list
+def dequeue(curr_queue):
+    item = -1
+    if not curr_queue.empty():
+        item = curr_queue.get()
+    else:
+        print("Queue is empty. Nothing to pull.")
+    return item, curr_queue
+
+
+# goal_node: Grid to match
+# curr_node: first item in list
+# Checks to make sure an end case has not been reached
+def compare(goal_node, curr_node):
+    if goal_node == curr_node:
+        print("Solution found!")
+
+
 def check_parity(user_start, user_end):
     temp_start = copy.copy(user_start)
     temp_end = copy.copy(user_end)
