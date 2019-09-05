d176e37d99c72f6b3cd2e340a732fe7fcb64c8c3
Alynoser <43915463+Alynoser@users.noreply.github.com>
2019-09-04 21:25:37 -0800

    Added in the user input for the different types of search algorithms

--- HW1/BreadthFirstSearch/main.py
+++ HW1/BreadthFirstSearch/main.py
@@ -1,7 +1,12 @@
 import sys
 import copy
+# user_start
+#  is the start point given by the user
+# user_end
+#  is the end point given by the user
 
-def checkparity(user_start, user_end):
+
+def check_parity(user_start, user_end):
     temp_start = copy.copy(user_start)
     temp_end = copy.copy(user_end)
     if 0 in temp_start:
@@ -26,11 +31,41 @@ def checkparity(user_start, user_end):
         print("YOU SUCK!!!")
         exit()
 
+def start_menu():
+    user_start1 = input("Please enter first line of the start sequence\n")
+    user_start2 = input("Enter second line\n")
+    user_start3 = input("Enter third line\n")
+    user_start = user_start1 + user_start2 + user_start3
+
+    user_end1 = input("Please enter the first line of the desired end set\n")
+    user_end2 = input("Enter second line\n")
+    user_end3 = input("Enter third line\n")
+    user_end = user_end1 + user_end2 + user_end3
+
+    user_start = list(map(int, user_start))
+    user_end = list(map(int, user_end))
+    check_parity(user_start, user_end)
+
+    while True:
+        print("1. Breadth First Search\n"
+              "2. Misplaced Tiles\n"
+              "3. Manhattan Distance\n"
+              "4. Gaschnig\n"
+              "5. Close program")
+        user_alg = int(input("Please choose an algorithm\n"))
+        if 5 > user_alg < 1:
+            print("You didnt not choose a valid algorithm")
+        elif user_alg == 1:
+            bredth_first_search(user_start, user_end)
+        elif user_alg == 2:
+            print("need to write")
+        elif user_alg == 3:
+            print("need to write")
+        elif user_alg == 4:
+            print("need to write")
+        elif user_alg == 5:
+            print("Thank you for being with us today")
+            exit()
 
 
-user_start = input("Please enter a starting set\n")
-user_end = input("Please enter desired end set\n")
-user_start = list(map(int, user_start))
-user_end = list(map(int, user_end))
-checkparity(user_start, user_end)
-
+start_menu()
