d559e1e2f6a5cd2ed96cccd1a4338537f4e38f9c
ArctiicChaos <ArctiicChaos@users.noreply.github.com>
2019-10-22 15:41:08 -0800

    >.>

--- Owari/Node.cpp
+++ Owari/Node.cpp
@@ -5,34 +5,91 @@ using namespace std;
 
 Node::Node()
 {
-public:
-	int depth = 0;
-	int alpha = 0;
-	int beta = 0;
-	
-	node* child1;
-	node* child2;
-	node* child3;
-	node* child4;
-	node* child5;
-	node* child6;
+	depth = -1;
+	alpha = -1;
+	beta = -1;
 }
 
-Node::Node(int depth)
+Node::Node(int depth, Board board)
 {
-public:
-	int depth = depth;
-	int alpha = 0;
-	int beta = 0;
-	node* child1;
-	node* child2;
-	node* child3;
-	node* child4;
-	node* child5;
-	node* child6;
+	depth = depth;
+	alpha = -1;
+	beta = -1;
+	this.board = board;
 }
 
 int Node::getDepth()
 {
 	return depth;
+}
+
+int Node::getAlpha()
+{
+	return alpha;
+}
+
+int Node::getBeta()
+{
+	return beta;
+}
+
+Node* Node::getNode(int num)
+{
+	if(num == 1)
+		return child1;
+	else if(num == 2)
+		return child2;
+	else if(num == 3)
+		return child3;
+	else if(num == 4)
+		return child4;
+	else if(num == 5)
+		return child5;
+	else if(num == 6)
+		return child6;
+	else
+		return prev_child;
+}
+
+Board Node::getBoard()
+{
+	return board;
+}
+
+void Node::setDepth(int mDepth)
+{
+	depth = mDepth;
+}
+
+void Node::setAlpha(int mAlpha)
+{
+	alpha = mAlpha;
+}
+
+void Node::setBeta(int mBeta)
+{
+	beta = mBeta;
+}
+
+void Node::setNode(int num, Node* child)
+{
+	if(num == 1)
+		child1 = child;
+	else if(num == 2)
+		child2 = child;
+	else if(num == 3)
+		child3 = child;
+	else if(num == 4)
+		child4 = child;
+	else if(num == 5)
+		child5 = child;
+	else if(num == 6)
+		child6 = child;
+	else
+		prev_child = child;
+}
+
+void Node::setBoard(Board board)
+{
+	this.board = board;
 }
